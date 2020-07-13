a47cc45ce12ad8e6e1974079bb2b871a90bb6e88
Jonathan Reyes <holostriker@gmail.com>
2020-07-13 19:53:36 -0400

    Add tests

---
+++ tests/integration/test_api_v5.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from twitch_chat_log_analyzer.api_v5 import TwitchClientv5
+from twitch_chat_log_analyzer.json_utils import load_json_file
+
+
+class TestTwitchAPIv5(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        data = load_json_file("creds.json")
+        cls.client_id = data["client_id"]
+
+    def test_initialize(self):
+        """Test Initialization of TwitchClientv5
+        """
+        _ = TwitchClientv5(self.client_id)
+
+    def test_get_chat_for_video(self):
+        video_id = "254274829"
+        twitch_client = TwitchClientv5(self.client_id)
+        data = twitch_client.get_chat_for_video(video_id)
+        self.assertIsNotNone(data)
