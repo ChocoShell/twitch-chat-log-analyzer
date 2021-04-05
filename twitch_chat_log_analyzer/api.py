daa356650d1db0b09e82e9fe3965007077e5ea4b
Jonathan Reyes <1192712+ChocoShell@users.noreply.github.com>
2021-04-04 21:51:36 -0400

    Add doc strings and fix missing parameters in TwitchClient

--- twitch_chat_log_analyzer/api.py
+++ twitch_chat_log_analyzer/api.py
@@ -8,10 +8,11 @@ def get_twitch_oauth_token(client_id, client_secret):
     """Get Twitch Access Token
 
     Args:
-        client_id: str
-        client_secret: str
+        client_id (str)
+        client_secret (str):
 
-    returns access_token: str
+    Returns:
+        (str): OAuth Access Token
     """
     params = {
         "grant_type": "client_credentials",
@@ -46,9 +47,33 @@ class TwitchAPI:
 
 
 class TwitchClient(TwitchAPI):
+    """
+    Client to connect to Twitch API from April 30, 2020 and manage OAuth
+
+    https://dev.twitch.tv/docs/api/reference
+
+    Attrs:
+        client_id (str)
+        client_secret (str)
+        token (str): OAuth Access Token
+        headers (dict): headers dictionary for Twitch API requests
+
+    Methods:
+        search_channels(query): Returns channel search results for given query
+        get_videos(
+            ids=None, user_id=None, game_id=None, **optional_query_params
+        ): Returns list of video metadata for given search parameters
+    """
     def __init__(self, client_id, client_secret):
+        """Stores client credentials and internal token parameter
+
+        Args:
+            client_id ([type]): [descripti (])on
+            client_secret ([type]): [descripti (])on
+        """
         self.client_id = client_id
         self.client_secret = client_secret
+        self._token = None
 
     @property
     def token(self):
