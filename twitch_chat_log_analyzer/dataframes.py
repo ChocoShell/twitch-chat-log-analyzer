039fdbb832daf43008422090fd567d1f3ac32292
Jonathan Reyes <1192712+ChocoShell@users.noreply.github.com>
2021-04-04 21:51:18 -0400

    Refactor dataframe function names

--- twitch_chat_log_analyzer/dataframes.py
+++ twitch_chat_log_analyzer/dataframes.py
@@ -7,17 +7,17 @@ from .json_utils import load_json_file
 # Pandas, FileIO, os searches
 
 
-def comment_data_to_df(comments):
+def convert_comments_to_df(comments):
     flattened_comments = [FilteredComment(comment).dict() for comment in comments]
     return pd.DataFrame(flattened_comments)
 
 
-def comment_data_to_csv(comments, csv_filename):
-    df = comment_data_to_df(comments)
+def convert_comments_to_csv(comments, csv_filename):
+    df = convert_comments_to_df(comments)
     df.to_csv(csv_filename, index=False)
 
 
-def comment_json_to_csv(comment_filename):
+def convert_comment_json_to_csv(comment_filename):
     comments = load_json_file(comment_filename)
     path, filename = os.path.split(comment_filename)
     name = filename.split(".")[0]
@@ -26,7 +26,7 @@ def comment_json_to_csv(comment_filename):
 
     csv_filename = os.path.join(path, csv_filename)
 
-    comment_data_to_csv(comments, csv_filename)
+    convert_comments_to_csv(comments, csv_filename)
 
 
 def convert_channel_comments_to_csv(channel):
@@ -36,6 +36,11 @@ def convert_channel_comments_to_csv(channel):
     for video_id in video_ids:
         comment_json_path = os.path.join(comments_dir, video_id, "comments.json")
         if os.path.isfile(comment_json_path):
-            comment_json_to_csv(comment_json_path)
+            convert_comment_json_to_csv(comment_json_path)
         else:
             print(f"File not found for video id: {video_id}")
+
+
+def get_comment_df_from_channel_video_id(channel, video_id):
+    csv_filename = f"./data/{channel}/comments/{video_id}/comments.csv"
+    return pd.read_csv(csv_filename)
