aee672061ceabfc17bb6181e394b5de96f8c7f86
Jonathan Reyes <1192712+ChocoShell@users.noreply.github.com>
2021-04-04 21:51:51 -0400

    Add get clips endpoint

---
+++ tests/integration/test_api.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+from twitch_chat_log_analyzer.api import TwitchAPI
+from twitch_chat_log_analyzer.json_utils import load_json_file
+
+
+class TestTwitchAPI(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        data = load_json_file("creds.json")
+        client_id = data["client_id"]
+        client_secret = data["client_secret"]
+        token = TwitchAPI.get_twitch_oauth_token(client_id, client_secret)
+        cls.api = TwitchAPI()
+        cls.api.headers = {
+            "Authorization": f"Bearer {token}",
+            "Client-ID": client_id,
+        }
+
+    def test_search_channels(self):
+        query = "lirik"
+        data = self.api.search_channels(query)
+        self.assertIsNotNone(data)
+
+    def test_get_videos(self):
+        user_id = "207813352"
+        data = self.api.get_videos(user_id=user_id)
+        self.assertIsNotNone(data)
+
+    def test_get_clips(self):
+        broadcaster_id = "207813352"
+        data = self.api.get_clips(broadcaster_id=broadcaster_id)
+        self.assertIsNotNone(data)
--- twitch_chat_log_analyzer/api.py
+++ twitch_chat_log_analyzer/api.py
@@ -1,8 +1,40 @@
+import requests
+
 from .base_api import BaseAPI
 
 
 class TwitchAPI(BaseAPI):
     base_twitch_url = "https://api.twitch.tv/helix"
+
+    @staticmethod
+    def get_twitch_oauth_token(client_id, client_secret):
+        """Get Twitch Access Token
+
+        Args:
+            client_id (str)
+            client_secret (str):
+
+        Returns:
+            (str): OAuth Access Token
+        """
+        params = {
+            "grant_type": "client_credentials",
+            "client_id": client_id,
+            "client_secret": client_secret,
+        }
+
+        twitch_oauth_url = "https://id.twitch.tv/oauth2/token"
+
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+
+        try:
+            response = requests.post(twitch_oauth_url, headers=headers, params=params)
+            response.raise_for_status()
+        except Exception as err:
+            print(err)
+            raise err
+
+        return response.json()["access_token"]
 
     def search_channels(self, query):
         url = f"{self.base_twitch_url}/search/channels"
@@ -27,6 +59,10 @@ class TwitchAPI(BaseAPI):
             List[dict]: List of dictionaries containing video metadata
         """
         if ids is None and user_id is None and game_id is None:
+            print(ids)
+            print(user_id)
+            print(game_id)
+            print(optional_query_params)
             raise Exception("Missing required query param: (ids, user_id, or game_id)")
 
         url = f"{self.base_twitch_url}/videos"
@@ -44,5 +80,42 @@ class TwitchAPI(BaseAPI):
 
         if game_id is not None:
             params["game_id"] = game_id
+
+        return self._handle_call("GET", url, params=params).json()
+
+    def get_clips(self, broadcaster_id=None, game_id=None, clip_ids=None):
+        """Gets clip information by clip ID (one or more),
+            broadcaster ID (one only), or game ID (one only).
+
+        The response has a JSON payload with a data field containing
+        an array of clip information elements and a pagination field
+        containing information required to query for more streams.
+
+        https://dev.twitch.tv/docs/api/reference#get-clips
+
+        Args:
+            broadcaster_id (str): ID of the broadcaster for whom clips are returned.
+                The number of clips returned is determined by
+                the first query-string parameter (default: 20).
+                Results are ordered by view count.
+            game_id (str): ID of the game for which clips are returned.
+                The number of clips returned is determined by
+                the first query-string parameter (default: 20).
+                Results are ordered by view count.
+            clip_id (List[str]): ID of the clip being queried. Limit: 100.
+        """
+        url = f"{self.base_twitch_url}/clips"
+
+        params = {}
+
+        if broadcaster_id:
+            params["broadcaster_id"] = broadcaster_id
+
+        if game_id:
+            params["game_id"] = game_id
+
+        if clip_ids:
+            clip_id_str = ",".join(clip_ids)
+            params["id"] = clip_id_str
 
         return self._handle_call("GET", url, params=params).json()
